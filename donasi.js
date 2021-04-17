d67a9ab27d1e467b3681c976f666a1a1dca27108
ferdiard <77181335+ferdiard@users.noreply.github.com>
2021-04-17 12:51:55 +0700

    Update donasi.js

--- donasi.js
+++ donasi.js
@@ -2,12 +2,10 @@ const donasi = () => {
 	return `       
 
 ◪ *DONASI*
-  ├─ ❏ *PULSA*
-  ├─ ❏ 0896-6626-2976
-  ├─ ❏ *SAWERIA*
-  ├─ ❏ Https://Saweria.co/FznXSec404
-  ├─ ❏ *INSTAGRAM BOT*
-  └─ ❏ https://instagram.com/miku.bot`
+  ❏ *PULSA*
+  ❏ 0821-6936-9877
+  ❏ *INSTAGRAM*
+  ❏ https://instagram.com/aardnnn`
 }
 
 exports.donasi = donasi
